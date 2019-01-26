10666a8510d9a4734dbf3331a5bac68fb5d3d67d
Hannah <hmansoori97@gmail.com>
2019-01-25 18:38:03 -0800

    Add begining of navbar

---
+++ photo-page/src/appBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
+function SimpleAppBar(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography className="classes.title" variant="h6" color="inherit">
+            Hannah Mansoori
+          </Typography>
+          <Typography variant="h9" color="inherit">
+            About
+            </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+SimpleAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleAppBar);
