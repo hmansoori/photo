3833d2c2073bc769dc08794367acf202e2d82a16
Hannah <hmansoori97@gmail.com>
2019-02-12 18:39:29 -0800

    Refactor application and add express

---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,14 @@
+  // Initialize Firebase
+  import firebase from 'firebase';
+  
+  const config = {
+    apiKey: "AIzaSyDm7PnQxiL-fXofz9ldiE4lLrjMGBMTPCM",
+    authDomain: "photo-page-8770a.firebaseapp.com",
+    databaseURL: "https://photo-page-8770a.firebaseio.com",
+    projectId: "photo-page-8770a",
+    storageBucket: "photo-page-8770a.appspot.com",
+    messagingSenderId: "337605064284"
+  };
+  firebase.initializeApp(config);
+
+  export default firebase;
